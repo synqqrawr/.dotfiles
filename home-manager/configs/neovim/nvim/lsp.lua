deb734636745351e62e466d230d5381ab855a28f
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-08-03 08:45:47 +0900

    ed/nvim: remove the "nightly" config
    Neovim on Nix is hard so I nuked it from existance

--- home-manager/configs/neovim/nvim/lsp.lua
+++
@@ -1,8 +0,0 @@
-local x = vim.diagnostic.severity
-
-vim.diagnostic.config {
-  virtual_text = { prefix = "" },
-  signs = { text = { [x.ERROR] = "󰅙", [x.WARN] = "", [x.INFO] = "󰋼", [x.HINT] = "󰌵" } },
-  underline = true,
-  float = { border = "single" },
-}
