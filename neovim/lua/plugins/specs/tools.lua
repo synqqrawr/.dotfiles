d2bd732d8e992b675698f1b781fef9d0a5e2c26f
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-08-06 12:39:39 +0900

    ed/nvim(gitgraph): init

--- neovim/lua/plugins/specs/tools.lua
+++ neovim/lua/plugins/specs/tools.lua
@@ -113,4 +113,23 @@ return {
       },
     },
   },
+  {
+    "isakbm/gitgraph.nvim",
+    dependencies = { "sindrets/diffview.nvim" },
+    opts = {
+      symbols = {
+        merge_commit = "M",
+        commit = "*",
+      },
+      format = {
+        timestamp = "%H:%M:%S %d-%m-%Y",
+        fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+      },
+    },
+    init = function()
+      vim.keymap.set("n", "<leader>gl", function()
+        require("gitgraph").draw({}, { all = true, max_count = 5000 })
+      end, { desc = "new git graph" })
+    end,
+  },
 }
