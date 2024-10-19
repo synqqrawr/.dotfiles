0ba08f0bc5253161c886f44aa9eab437de22d013
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-10-19 13:54:35 +0900

    feat: Use a unified nixpkgs module for nixpkgs options

---
+++ nixos/config/nix.nix
@@ -0,0 +1,24 @@
+{ outputs, inputs, ... }:
+{
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.modifications
+
+      # You can also add overlays exported from other flakes:
+      inputs.neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+}
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -31,29 +31,8 @@
     ./config/fcitx5.nix
     ./config/keyboard.nix
     ./config/power.nix
+    ./config/nix.nix
   ];
-
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-
-      # You can also add overlays exported from other flakes:
-      inputs.neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    config = {
-      allowUnfree = true;
-    };
-  };
 
   nix =
     let
