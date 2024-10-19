721351e1aac38878eb773f97e4cf87cd2b882121
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-10-19 13:15:36 +0900

    refactor(firefox): Remove boilerplate via Nix functions
    - Use builtin Nix functions and maps to decrease the boilerplate in the
    file.
    
    As of right now, this doesn't reduce that much boilerplate but as I
    start adding more searx rules, it should reduce the LOC and more
    importantly the characters in the file, leading to a possible decrease
    in eval time.

--- home-manager/configs/firefox.nix
+++ home-manager/configs/firefox.nix
@@ -1,3 +1,7 @@
+# WELCOME TO HELL, BABY
+# This file has 400LOC, and guess what -- I'm too lazy to modualize this.
+# Please help me. Also the comments add 3 more LOC.
+
 {
   inputs,
   lib,
@@ -50,58 +54,73 @@ in
             "behind-the-scene * image noop"
             "behind-the-scene * 3p noop"
           ];
-          userFilters = lib.concatMapStrings (x: x + "\n") [
-            "||0.0.0.0^$3p,domain=~localhost|~127.0.0.1|~[::1]|~0.0.0.0|~[::]|~local "
+          userFilters =
+            let
+              # Base filters
+              baseFilters = [
+                "||0.0.0.0^$3p,domain=~localhost|~127.0.0.1|~[::1]|~0.0.0.0|~[::]|~local"
+                "en.wikipedia.org##+js(trusted-set-cookie, enwikimwclientpreferences, skin-theme-clientpref-night%2Cvector-feature-limited-width-clientpref-1%2Cvector-feature-custom-font-size-clientpref-1%2Cvector-feature-appearance-pinned-clientpref-0)"
+                "ja.wikipedia.org##+js(trusted-set-cookie, jawikimwclientpreferences, skin-theme-clientpref-night%2Cvector-feature-limited-width-clientpref-1%2Cvector-feature-custom-font-size-clientpref-1%2Cvector-feature-appearance-pinned-clientpref-0)"
+              ];
 
-            "en.wikipedia.org##+js(trusted-set-cookie, enwikimwclientpreferences, skin-theme-clientpref-night%2Cvector-feature-limited-width-clientpref-1%2Cvector-feature-custom-font-size-clientpref-1%2Cvector-feature-appearance-pinned-clientpref-0)"
-            "ja.wikipedia.org##+js(trusted-set-cookie, jawikimwclientpreferences, skin-theme-clientpref-night%2Cvector-feature-limited-width-clientpref-1%2Cvector-feature-custom-font-size-clientpref-1%2Cvector-feature-appearance-pinned-clientpref-0)"
-            "searx.be##+js(trusted-set-cookie, categories, general)"
+              # YouTube filters
+              youtubeFilters = [
+                ''
+                  ! YT Homepage and Subscriptions (Grid View) - Hide the Shorts section
+                  youtube.com##[is-shorts]
+                  ! YT Menu - Hide the Shorts button
+                  www.youtube.com###guide [title="Shorts"], .ytd-mini-guide-entry-renderer[title="Shorts"]
+                  ! YT Search - Hide Shorts
+                  www.youtube.com##ytd-search ytd-video-renderer:has([overlay-style="SHORTS"])
+                  ! YT Search, Channels, Subscriptions (List View) and Sidebar/Below Player Recommendations - Hide the Shorts sections
+                  www.youtube.com##ytd-reel-shelf-renderer
+                  ! YT Channels - Hide the Shorts tab
+                  www.youtube.com##[tab-title="Shorts"]
+                  ! YT Subscriptions - Hide Shorts - Grid View
+                  www.youtube.com##ytd-browse[page-subtype="subscriptions"] ytd-grid-video-renderer:has([overlay-style="SHORTS"])
+                  ! YT Subscriptions - Hide Shorts - List View
+                  www.youtube.com##ytd-browse[page-subtype="subscriptions"] ytd-video-renderer:has([overlay-style="SHORTS"])
+                  ! YT Subscriptions - New Layout - Hide Shorts
+                  www.youtube.com##ytd-browse[page-subtype="subscriptions"] ytd-rich-item-renderer:has([overlay-style="SHORTS"])
+                  ! YT Sidebar - Hide Shorts
+                  www.youtube.com###related ytd-compact-video-renderer:has([overlay-style="SHORTS"])
+                  ! YT Mobile - Hide the Shorts Menu button
+                  m.youtube.com##ytm-pivot-bar-item-renderer:has(>.pivot-shorts)
+                  ! YT Mobile - Hide the Shorts sections
+                  m.youtube.com##ytm-reel-shelf-renderer
+                  m.youtube.com##ytm-rich-section-renderer:has([d^="M17.77,10.32l-1.2"])
+                  ! YT Mobile - Hide Shorts in search results
+                  m.youtube.com##ytm-search ytm-video-with-context-renderer:has([data-style="SHORTS"])
+                  ! YT Mobile - Hide the Shorts button on Channels
+                  m.youtube.com##[tab-title="Shorts"]
+                ''
+                ''
+                  ! YT Search - keep only videos (no shorts)
+                  youtube.com##ytd-search ytd-item-section-renderer>#contents>:is(:not(ytd-video-renderer,yt-showing-results-for-renderer,[icon-name="promo-full-height:EMPTY_SEARCH"]),ytd-video-renderer:has([aria-label="Shorts"])),ytd-secondary-search-container-renderer
 
-            ''
-              ! YT Homepage and Subscriptions (Grid View) - Hide the Shorts section
-              youtube.com##[is-shorts]
-              ! YT Menu - Hide the Shorts button
-              www.youtube.com###guide [title="Shorts"], .ytd-mini-guide-entry-renderer[title="Shorts"]
-              ! YT Search - Hide Shorts
-              www.youtube.com##ytd-search ytd-video-renderer:has([overlay-style="SHORTS"])
-              ! YT Search, Channels, Subscriptions (List View) and Sidebar/Below Player Recommendations - Hide the Shorts sections
-              www.youtube.com##ytd-reel-shelf-renderer
-              ! YT Channels - Hide the Shorts tab
-              www.youtube.com##[tab-title="Shorts"]
-              ! YT Subscriptions - Hide Shorts - Grid View
-              www.youtube.com##ytd-browse[page-subtype="subscriptions"] ytd-grid-video-renderer:has([overlay-style="SHORTS"])
-              ! YT Subscriptions - Hide Shorts - List View
-              www.youtube.com##ytd-browse[page-subtype="subscriptions"] ytd-video-renderer:has([overlay-style="SHORTS"])
-              ! YT Subscriptions - New Layout - Hide Shorts
-              www.youtube.com##ytd-browse[page-subtype="subscriptions"] ytd-rich-item-renderer:has([overlay-style="SHORTS"])
-              ! YT Sidebar - Hide Shorts
-              www.youtube.com###related ytd-compact-video-renderer:has([overlay-style="SHORTS"])
+                  ! YT Search - keep only videos (no shorts) and channels
+                  youtube.com##ytd-search ytd-item-section-renderer>#contents>:is(:not(ytd-video-renderer,ytd-channel-renderer,yt-showing-results-for-renderer,[icon-name="promo-full-height:EMPTY_SEARCH"]),ytd-video-renderer:has([aria-label="Shorts"])),ytd-secondary-search-container-renderer
 
-              ! YT Mobile - Hide the Shorts Menu button
-              m.youtube.com##ytm-pivot-bar-item-renderer:has(>.pivot-shorts)
-              ! YT Mobile - Hide the Shorts sections
-              m.youtube.com##ytm-reel-shelf-renderer
-              m.youtube.com##ytm-rich-section-renderer:has([d^="M17.77,10.32l-1.2"])
-              ! YT Mobile - Hide Shorts in search results
-              m.youtube.com##ytm-search ytm-video-with-context-renderer:has([data-style="SHORTS"])
-              ! YT Mobile - Hide the Shorts button on Channels
-              m.youtube.com##[tab-title="Shorts"]
-            ''
-            ''
-              ! YT Search - keep only videos (no shorts)
-              youtube.com##ytd-search ytd-item-section-renderer>#contents>:is(:not(ytd-video-renderer,yt-showing-results-for-renderer,[icon-name="promo-full-height:EMPTY_SEARCH"]),ytd-video-renderer:has([aria-label="Shorts"])),ytd-secondary-search-container-renderer
+                  ! YT Search - keep only videos (no shorts), channels and playlists
+                  youtube.com##ytd-search ytd-item-section-renderer>#contents>:is(:not(ytd-video-renderer,ytd-channel-renderer, ytd-playlist-renderer,yt-showing-results-for-renderer,[icon-name="promo-full-height:EMPTY_SEARCH"]),ytd-video-renderer:has([aria-label="Shorts"])),ytd-secondary-search-container-renderer
+                ''
+                ''
+                  ! YT Sidebar - Hide videos based on their titles
+                  youtube.com###related ytd-compact-video-renderer:has(#video-title:is([title*="mrbeast"i]))
+                ''
+              ];
 
-              ! YT Search - keep only videos (no shorts) and channels
-              youtube.com##ytd-search ytd-item-section-renderer>#contents>:is(:not(ytd-video-renderer,ytd-channel-renderer,yt-showing-results-for-renderer,[icon-name="promo-full-height:EMPTY_SEARCH"]),ytd-video-renderer:has([aria-label="Shorts"])),ytd-secondary-search-container-renderer
+              # Add cookie settings to filters
+              cookieFilters = map (attr: "${searx}##+js(trusted-set-cookie, ${attr})") [
+                "categories, general"
+                "language, all"
+              ];
 
-              ! YT Search - keep only videos (no shorts), channels and playlists
-              youtube.com##ytd-search ytd-item-section-renderer>#contents>:is(:not(ytd-video-renderer,ytd-channel-renderer, ytd-playlist-renderer,yt-showing-results-for-renderer,[icon-name="promo-full-height:EMPTY_SEARCH"]),ytd-video-renderer:has([aria-label="Shorts"])),ytd-secondary-search-container-renderer
-            ''
-            ''
-              ! YT Sidebar - Hide videos based on their titles
-              youtube.com###related ytd-compact-video-renderer:has(#video-title:is([title*="mrbeast"i]))
-            ''
-          ];
+              # Combining all filters into a single list
+              allFilters = baseFilters ++ youtubeFilters ++ cookieFilters;
+
+            in
+            allFilters; # Returns a list of strings
           hostnameSwitchesString = lib.concatMapStrings (x: x + "\n") [
             "no-large-media: behind-the-scene false"
             "no-csp-reports: * true"
@@ -193,28 +212,10 @@ in
       };
     };
     profiles = {
-      test = {
-        name = "test";
-        id = 1;
-        userChrome = ''
-          @import "${inputs.shyfox}/chrome/userChrome.css";
-        '';
-        userContent = ''
-          @import "${inputs.shyfox}/chrome/userContent.css"
-        '';
-        extraConfig = ''
-          user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-          user_pref("svg.context-properties.content.enabled", true);
-          user_pref("layout.css.color-mix.enabled", true);
-          user_pref("layout.css.light-dark.enabled", true);
-          user_pref("layout.css.has-selector.enabled", true);
-        '';
-      };
       async = {
         name = "async";
         id = 0;
         userChrome =
-          # css
           ''
             @import "${inputs.shyfox}/chrome/userChrome.css";
 
@@ -299,41 +300,6 @@ in
           default = "SearXNG (${searx})";
           force = true;
           engines = {
-            "Brave" = {
-              urls = [
-                {
-                  template = "https://search.brave.com/search?q={searchTerms}";
-                  params = [
-                    {
-                      name = "q";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              icon = "${pkgs.brave}/share/icons/hicolor/64x64/apps/brave-browser.png";
-              definedAliases = [
-                # fuck you, bing. i aint using you
-                "!b"
-                "@brave"
-              ];
-            };
-            "Noogle" = {
-              urls = [
-                {
-                  template = "https://noogle.dev/q";
-                  params = [
-                    {
-                      name = "term";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              iconUpdateURL = "https://noogle.dev/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # every day
-              definedAliases = [ "@ng" ];
-            };
             "MyNixOS" = {
               urls = [
                 {
@@ -380,20 +346,6 @@ in
               ];
               definedAliases = [ "@s" ];
             };
-            "Marginalia.nu" = {
-              urls = [
-                {
-                  template = "https://search.marginalia.nu/search?query={searchTerms}";
-                  params = [
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              definedAliases = [ "@mn" ];
-            };
             "Wikipedia (en)".metaData.alias = "@wiki";
             "Google".metaData.hidden = true;
             "Amazon.com".metaData.hidden = true;
@@ -424,13 +376,6 @@ in
 
             user_pref("browser.download.useDownloadDir", false);
             user_pref("accessibility.force_disabled", 1);
-            // user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"_3c078156-979c-498b-8990-85f7987dd929_-browser-action\",\"atbc_easonwong-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"_c2c003ee-bd69-42a2-b0e9-6f34222cb046_-browser-action\",\"_cb31ec5d-c49a-4e5a-b240-16c767444f62_-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"userchrome-toggle-extended_n2ezr_ru-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"dearrow_ajay_app-browser-action\",\"sponsorblocker_ajay_app-browser-action\",\"magnolia_12_34-browser-action\",\"customizableui-special-spring8\",\"fullscreen-button\",\"screenshot-button\",\"firefox-view-button\",\"downloads-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[\"ublock0_raymondhill_net-browser-action\",\"_3c078156-979c-498b-8990-85f7987dd929_-browser-action\",\"sponsorblocker_ajay_app-browser-action\",\"developer-button\",\"atbc_easonwong-browser-action\",\"magnolia_12_34-browser-action\",\"userchrome-toggle-extended_n2ezr_ru-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"_c2c003ee-bd69-42a2-b0e9-6f34222cb046_-browser-action\",\"_cb31ec5d-c49a-4e5a-b240-16c767444f62_-browser-action\",\"dearrow_ajay_app-browser-action\"],\"dirtyAreaCache\":[\"unified-extensions-area\",\"nav-bar\",\"PersonalToolbar\",\"TabsToolbar\",\"toolbar-menubar\"],\"currentVersion\":20,\"newElementCount\":10}");
-            // user_pref("browser.theme.toolbar-theme", 0);
-            // user_pref("browser.theme.content-theme", 0);
-            // user_pref("browser.tabs.inTitlebar", 0);
-            // user_pref("browser.uidensity", 0);
-            // user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
-
           ''
         ];
       };
