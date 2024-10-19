f3d1b56d45ad46bf610aba68fda9c447265a0504
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-10-19 13:50:37 +0900

    fix: Remove the unneeded SF Pro package and overlays
    I don't need SF Pro anymore and thus and therefore I removed the whole
    overlay for it.
    I also removed ./pkgs/ as I don't use it anymore.

--- pkgs/default.nix
+++
@@ -1,3 +0,0 @@
-# Custom packages, that can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example'
-pkgs: { custom-fonts = pkgs.callPackage ./custom-fonts/default.nix { inherit pkgs; }; }
