046b2f3a4af57b9b30f6f3e6f3810ac6e3eb4109
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-11-05 18:30:41 +0900

    remove pyprland

--- home-manager/configs/hypr.nix
+++ home-manager/configs/hypr.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  config,
   ...
 }: {
   imports = [
@@ -11,21 +10,13 @@
   ];
   home.packages = [
     pkgs.grimblast
-    pkgs.pyprland
   ];
-  home.file."${config.xdg.configHome}/hypr/pyprland.toml".text = ''
-    [pyprland]
-    plugins = [
-        "magnify",
-    ]
-  '';
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
       exec-once = [
         "fcitx5 -d"
         "ags"
-        "${pkgs.pyprland}/bin/pypr &"
       ];
       monitor = ",preferred,auto,1";
       general = {
