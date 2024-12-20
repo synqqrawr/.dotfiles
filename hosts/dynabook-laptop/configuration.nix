e77113fc5810096f316f7250b826358a43e9b33f
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-12-20 16:04:00 +0900

    ghostty: update inputs

--- hosts/dynabook-laptop/configuration.nix
+++ hosts/dynabook-laptop/configuration.nix
@@ -71,17 +71,6 @@
   # enable fish vendor completions
   programs.fish.enable = true;
 
-  nix = {
-    settings = {
-      substituters = ["https://ghostty.cachix.org"];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc"
-        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
-      ];
-    };
-  };
-
   environment.systemPackages = with pkgs; [
     keepassxc
     sqlite
@@ -100,6 +89,7 @@
     nvtopPackages.intel
     inputs.neovim-nightly-overlay.packages.${pkgs.system}.default
     inputs.ghostty.packages.x86_64-linux.default
+    cachix
   ];
 
   services.flatpak.enable = true;
